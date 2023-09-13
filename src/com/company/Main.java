31d800da2d07dea1c72e730e000712e45bb16dd9
VengerovskayaAlena <31alena_vengerovskaya@mail.ru>
2023-09-13 19:45:50 +0300

    homework6

---
+++ src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println(" Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    System.out.println(" Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+    System.out.println(" Задача 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+
+    System.out.println(" Задача 4");
+        for (int i = -10; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    System.out.println(" Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+    System.out.println(" Задача 6");
+        for (int i = 7; i < 100; i = i + 7) {
+            System.out.println(i);
+        }
+
+    System.out.println(" Задача 7");
+        for (int i = 1; i < 1000; i = i*2) {
+            System.out.println(i);
+        }
+
+    System.out.println(" Задача 8");
+        int savings = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + savings;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+    System.out.println(" Задача 9");
+        int savings2 = 29000;
+        int total2 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total2 = total2 + total2/100;
+            total2 = total2 + savings2;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total2 + " рублей");
+        }
+
+    System.out.println(" Задача 10");
+        int a = 0;
+        for (int i = 1; i <= 10; i++) {
+            a = 2 * i;
+            System.out.println("2*" + i + "=" + a);
+        }
+    }
+}
